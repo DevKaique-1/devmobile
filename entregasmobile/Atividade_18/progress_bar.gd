2e33206d2c990c7a2238600cf545cf1a315622da
kaique <kaquerocha89@gmail.com>
2025-09-30 11:46:55 -0300

    att_particullas

--- entregasmobile/Atividade_18/progress_bar.gd
+++ entregasmobile/Atividade_18/progress_bar.gd
@@ -1,7 +1,8 @@
 extends Control
-
+var temp = 0;
 func _on_timer_timeout() -> void:
 	$TextureProgressBar.value +=1
+	
 	pass
 
 
@@ -12,7 +13,18 @@ func _on_button_pressed() -> void:
 
 func _on_texture_progress_bar_value_changed(value: float) -> void:
 	if value >= $TextureProgressBar.max_value:
+		
 		$TextureProgressBar.max_value  = $TextureProgressBar.max_value *1.2
 		$TextureProgressBar.value = 0
 		$TextureProgressBar/Label.text = str(int($TextureProgressBar/Label.text) +1)
+		temp += 1
+		if temp == 1:
+			$CPUParticles2D.emitting = true
+			temp = 0
+			pass
+		if temp == 0:
+			$CPUParticles2D.emitting = false
+			pass
+		pass
+	pass
 	pass # Replace with function body.
