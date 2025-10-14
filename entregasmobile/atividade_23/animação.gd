6ef3c61de70b3c917e106ce713c6b10a594b9d5c
kaique <kaquerocha89@gmail.com>
2025-10-14 12:05:53 -0300

    atts_23_24

--- entregasmobile/atividade_23/animação.gd
+++ entregasmobile/atividade_23/animação.gd
@@ -1,7 +1,12 @@
 extends Node2D
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var _animated_sprite = $AnimatedSprite2D 
+ 
+func _ready():
+	_animated_sprite.play("idle")
+	
+
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_aceept"):
+	if Input.is_action_pressed("ui_up"):
 		_animated_sprite.play("run")
 	else:
 		_animated_sprite.stop()
